508743c80146629af80a2f8f558d72e84441c8e9
Julian Thijssen <Nimthora@gmail.com>
2018-07-28 17:05:38 +0200

    Add Input interfaces

---
+++ Source/Input.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class KeyListener
+{
+public:
+    virtual void onKeyPressed(int key, int mods) = 0;
+    virtual void onKeyReleased(int key, int mods) = 0;
+};
+
+class MouseMoveListener
+{
+public:
+    virtual void onMouseMove(float x, float y) = 0;
+};
+
+class MouseClickListener
+{
+public:
+    virtual void onMouseClicked(int button, int mods) = 0;
+    virtual void onMouseReleased(int button, int mods) = 0;
+};
