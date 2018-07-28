02c032de145a11e5677015745d007228a227cbd4
Julian Thijssen <Nimthora@gmail.com>
2018-07-28 17:02:24 +0200

    Add input listeners to the window

--- Source/Window.cpp
+++ Source/Window.cpp
@@ -6,6 +6,22 @@
 void onError(int error, const char* description)
 {
     fprintf(stderr, "Error: %s\n", description);
+void onKeyEvent(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    Window* w = reinterpret_cast<Window*>(glfwGetWindowUserPointer(window));
+    w->onKeyInput(key, action, mods);
+}
+
+void onMouseMoveEvent(GLFWwindow* window, double x, double y)
+{
+    Window* w = reinterpret_cast<Window*>(glfwGetWindowUserPointer(window));
+    w->onMouseMove(x, y);
+}
+
+void onMouseButtonEvent(GLFWwindow* window, int button, int action, int mods)
+{
+    Window* w = reinterpret_cast<Window*>(glfwGetWindowUserPointer(window));
+    w->onMouseButton(button, action, mods);
 }
 
 uint Window::getWidth() const
@@ -41,6 +57,10 @@ void Window::create(std::string title, uint width, uint height)
 
     glfwSwapInterval(1);
 
+    glfwSetWindowUserPointer(window, this);
+    glfwSetKeyCallback(window, onKeyEvent);
+    glfwSetCursorPosCallback(window, onMouseMoveEvent);
+    glfwSetMouseButtonCallback(window, onMouseButtonEvent);
     // If we failed to create a window, throw an exception
     if (!window)
         throw WindowCreationException("Failed to create GLFW window");
@@ -67,4 +87,51 @@ void Window::destroy()
     glfwDestroyWindow(window);
 
     glfwTerminate();
+}
+void Window::addKeyListener(KeyListener* keyListener)
+{
+    keyListeners.push_back(keyListener);
+}
+
+void Window::addMouseMoveListener(MouseMoveListener* mouseMoveListener)
+{
+    mouseMoveListeners.push_back(mouseMoveListener);
+}
+
+void Window::addMouseClickListener(MouseClickListener* mouseClickListener)
+{
+    mouseClickListeners.push_back(mouseClickListener);
+}
+
+void Window::onKeyInput(int key, int action, int mods)
+{
+    //keyListeners.erase(std::remove_if(keyListeners.begin(), keyListeners.end(),
+    //    [](KeyListener* listener) { return listener == nullptr; }), keyListeners.end());
+
+    for (KeyListener* listener : keyListeners)
+    {
+        if (action == GLFW_PRESS)
+            listener->onKeyPressed(key, mods);
+        else
+            listener->onKeyReleased(key, mods);
+    }
+}
+
+void Window::onMouseMove(float x, float y)
+{
+    for (MouseMoveListener* listener : mouseMoveListeners)
+    {
+        listener->onMouseMove(x, y);
+    }
+}
+
+void Window::onMouseButton(int button, int action, int mods)
+{
+    for (MouseClickListener* listener : mouseClickListeners)
+    {
+        if (action == GLFW_PRESS)
+            listener->onMouseClicked(button, mods);
+        else
+            listener->onMouseReleased(button, mods);
+    }
 }
--- Source/Window.h
+++ Source/Window.h
@@ -1,7 +1,10 @@
 #pragma once
 
-#include <exception>
+#include "Input.h"
+
+#include <vector>
 #include <string>
+#include <exception>
 
 typedef unsigned int uint;
 
@@ -35,8 +38,21 @@ public:
     void close();
     void destroy();
 
+    void addKeyListener(KeyListener* keyListener);
+    void addMouseMoveListener(MouseMoveListener* mouseMoveListener);
+    void addMouseClickListener(MouseClickListener* mouseClickListener);
+
+    void onKeyInput(int key, int action, int mods);
+    void onMouseMove(float x, float y);
+    void onMouseButton(int button, int action, int mods);
+
 private:
     GLFWwindow* window;
 
     uint _width, _height;
+
+    // Non-owning lists of input listener pointer references
+    std::vector<KeyListener*> keyListeners;
+    std::vector<MouseMoveListener*> mouseMoveListeners;
+    std::vector<MouseClickListener*> mouseClickListeners;
 };
