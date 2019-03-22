b6f52fbf6eebce90143c91c17f672dd281c94f72
Julian Thijssen <Nimthora@gmail.com>
2019-03-22 13:02:21 +0100

    Move File code to implementation file

---
+++ Source/File.cpp
@@ -0,0 +1,30 @@
+#include "File.h"
+
+FileNotFoundException::FileNotFoundException(std::string filePath) :
+    _errorMessage(std::string("Failed to find file: ") + filePath)
+{
+
+}
+
+const char* FileNotFoundException::what() const throw()
+{
+    return _errorMessage.c_str();
+}
+
+std::string loadFile(std::string filePath)
+{
+    std::ifstream file(filePath);
+    if (file.fail() || !file.is_open())
+    {
+        throw FileNotFoundException(filePath);
+    }
+
+    std::string source;
+    std::string line;
+
+    while (getline(file, line))
+    {
+        source.append(line + '\n');
+    }
+    return source;
+}
--- Source/File.h
+++ Source/File.h
@@ -7,33 +7,12 @@
 
 struct FileNotFoundException : std::exception
 {
-    FileNotFoundException(std::string filePath)
-        : _errorMessage(std::string("Failed to find file: ") + filePath)
-    { }
+    FileNotFoundException(std::string filePath);
 
-    const char* what() const throw()
-    {
-        return _errorMessage.c_str();
-    }
+    const char* what() const throw();
 
 private:
     std::string _errorMessage;
 };
 
-std::string loadFile(std::string filePath)
-{
-    std::ifstream file(filePath);
-    if (file.fail() || !file.is_open())
-    {
-        throw FileNotFoundException(filePath);
-    }
-
-    std::string source;
-    std::string line;
-
-    while (getline(file, line))
-    {
-        source.append(line + '\n');
-    }
-    return source;
-}
+std::string loadFile(std::string filePath);
