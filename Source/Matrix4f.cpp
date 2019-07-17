34d4d0a6bd61e9a83d2c90f593671fd19b95a035
Julian Thijssen <Nimthora@gmail.com>
2019-07-18 00:36:36 +0200

    Add stream output operator on Matrix4f

--- Source/Matrix4f.cpp
+++ Source/Matrix4f.cpp
@@ -271,16 +271,16 @@ namespace GDT
 
         return d;
     }
-}
 
-std::ostream& operator<<(std::ostream& os, const Matrix4f& m)
-{
-    os << "[" << m[0] << ", " << m[4] << ", " << m[8] << ", " << m[12] << "]\n";
-    os << "[" << m[1] << ", " << m[5] << ", " << m[9] << ", " << m[13] << "]\n";
-    os << "[" << m[2] << ", " << m[6] << ", " << m[10] << ", " << m[14] << "]\n";
-    os << "[" << m[3] << ", " << m[7] << ", " << m[11] << ", " << m[15] << "]\n";
+    std::ostream& operator<<(std::ostream& os, const Matrix4f& m)
+    {
+        os << "[" << m[0] << ", " << m[4] << ", " << m[8] << ", " << m[12] << "]\n";
+        os << "[" << m[1] << ", " << m[5] << ", " << m[9] << ", " << m[13] << "]\n";
+        os << "[" << m[2] << ", " << m[6] << ", " << m[10] << ", " << m[14] << "]\n";
+        os << "[" << m[3] << ", " << m[7] << ", " << m[11] << ", " << m[15] << "]\n";
 
-    return os;
+        return os;
+    }
 #ifdef GDT_NAMESPACE
 }
 #endif
